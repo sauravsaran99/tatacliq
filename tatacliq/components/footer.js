cf1921e0fdd1b271662350a5fc8efdf660ddcb04
Saurav Kumar <66282953+sauravsaran99@users.noreply.github.com>
2022-01-20 13:27:14 +0530

    Add files via upload

---
+++ tatacliq/components/footer.js
@@ -0,0 +1,175 @@
+function footer(){
+    return `<footer>
+            <div id="details">
+                <div id="flex">
+                <h4>Tata Trust</h4>
+                <h4>QuiQ PiQ</h4>
+                <h4>authentic Brands</h4>
+                <h4>Easy Returns</h4>
+            </div>
+            </div>
+            <div id="list">
+                <div id="box1">
+                    <h4>Tata MarketPlace</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#">Careers</a>
+                        </li>
+                        <li>
+                            <a href="#">Sell With Us</a>
+                        </li>
+                        <li>
+                            <a href="#">Terms of Use</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="#">Affiliates</a>
+                        </li>
+                        <li>
+                            <a href="#">Sitemap</a>
+                        </li>
+                    </ul>
+                </div>
+                <div id="box2">
+                    <h4>Customer Service</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#">Shopping</a>
+                        </li>
+                        <li>
+                            <a href="#">Frequently Asked Questions</a>
+                        </li>
+                        <li>
+                            <a href="#">Offers & Promotions</a>
+                        </li>
+                        <li>
+                            <a href="#">Payments</a>
+                        </li>
+                        <li>
+                            <a href="#">Cancellation</a>
+                        </li>
+                        <li> 
+                            <a href="#">Returns & Refunds</a>
+                        </li>
+                        <li>
+                            <a href="#">CliQ And PiQ</a>
+                        </li>
+                        <li>
+                            <a href="#">Electronics return Policy</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact Us</a>
+                        </li>
+                        <li>
+                            <a href="#">Reviews Guidelines</a>
+                        </li>
+                    </ul>
+                </div>
+                <div id="box3">
+                    <h4>My Tata CLiQ</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#">My Account</a>
+                        </li>
+                        <li>
+                            <a href="#">My Orders</a>
+                        </li>
+                        <li>
+                            <a href="#">My shopping Bag</a>
+                        </li>
+                        <li>
+                            <a href="#">My Wishlist</a>
+                        </li>
+                    </ul>
+                </div>
+                <div id="box4">
+                    <h4>Tata CLiQ Offerings</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#">Westside</a> |   <a href="#">Sweaters</a> |  <a href="#">Pantaloons</a> |
+                            <a href="#">Fastrack Watches</a> | <a href="#">Red Tape Shoes</a> | <a href="#">Smart Watch</a> |
+                            <a href="#">Watches</a> | <a href="#">Leggings</a> | <a href="#">Biba</a> | <a href="#">Sweater for Women</a> |
+                            <a href="#">Adidas</a> | <a href="#">TV</a> | <a href="#">T Shirt</a> | <a href="#">Shoes for Men</a> |
+                            <a href="#">Handbags</a> | <a href="#">Earphones</a> | <a href="#">Power Bank</a> | <a href="#">Home Theatre</a> |
+                            <a href="#">Fridge</a> | <a href="#">Laptop</a> | <a href="#">Boat headphones</a> | <a href="#">Refrigerators</a> |
+                            <a href="#">AC</a> | <a href="#">Microwave Oven</a> | <a href="#">red Cheif Shoes</a> | <a href="#">Mobile Phones</a> |
+                            <a href="#">Puma Shoes</a> | <a href="#">Skinn by Titan</a> | <a href="#">Sports Shoes</a> |
+                            <a href="#">Ajmal Perfumes</a> | <a href="#">Beardo</a> | <a href="#">Washing Machine</a> | <a href="#">Realme Mobile Price</a> |
+                            <a href="#">Mamaearth</a> | <a href="#">Shoes for Women</a> | <a href="#">Earphone</a> | <a href="#">AND</a> |
+                            <a href="#">Boat Earphones</a> | <a href="#">Forever 21</a> | <a href="#">Titan Watches</a> | <a href="#">JBL</a> |  <a href="#">Levis</a>
+                        </li> 
+                    </ul>
+                </div>
+            </div>
+            <div id="footer">
+                <div id="foot_top">
+                    <div id="app">
+                        <div id="size">Download App</div>
+                        <div><i class="fa fa-android" style="font-size: 20px;"></i></div>
+                        <div><i class='fab fa-apple' style="font-size: 20px;"></i></div>
+                    </div>
+                    <div id="icon">
+                        <div><i class="fab fa-facebook-square" style="font-size: 22px;"></i></div>
+                        <div><i class="fab fa-instagram-square" style="font-size: 22px;"></i></div>
+                        <div><i class="fab fa-twitter-square" style="font-size: 22px;"></i></div>
+                        <div><i class="fab fa-youtube" style="font-size: 22px;"></i></div>
+                        <div><i class='fa fa-linkedin-square' style="font-size: 22px;"></i></div>
+                    </div id="tata">
+                    <div>© 2022 Tata CLiQ | All rights reserved</div>
+                </div>
+            </div>
+            <div id="foot">
+                <b>TATA CLiQ: Shop Online with India's most trusted destination. Buy Brands you love at prices you
+                    want!</b><br>
+                Shop online whenever you want to - yes, that 24x7 experience is so convenient now, isn't it?! Get
+                genuine products from all the Top Brands delivered to your doorstep, with free shipping. Our clean,
+                immersive design allows for easy navigation across categories and brand stores so you can find the best
+                products from a wide range of <a href="#">smartphones</a>, <a href="#">mobile phones</a>, <a
+                    href="#">laptops</a>, <a href="#">women's clothing</a>, <a href="#">men's clothing</a>,
+                <a href="#">kids wear</a>, <a href="#">footwear</a>, <a href="#">watches</a> and <a href="#">accessories</a>
+                online. You can also check our great <a href="#">offers</a> or <a href="#">deal of the
+                    day</a> section and get the best prices on various products across lifestyle, fashion, electronics,
+                appliances & more. With such offerings and TATA CLiQ exclusive bank offers and TATA CLiQ coupons, do
+                remeber to buy mobile online, do online electronics shopping and online dress shopping of top Brands at
+                the best prices available throughout the year<br>
+                <b>Online Shopping: Fast & convenient with the click of a button</b><br>
+                The upside of online shopping at TATA CLiQ online store, is that you'll save on time and most
+                importantly money with TataCliq offers. It's as simple as comparing products and prices online before
+                making the right buy. What's more, you also have the option to pay for your favourite brands and
+                products using our easy EMI options. Of course, you can buy and try - in the convenience of your home.
+                Returns are easy too: We'll pick up your returns for free or you can also drop off the goods at the
+                nearest brand store.<br>
+                <b>Tata CLiQ Shopping App: Everything you want with just a few clicks on Android & iOS</b><br>
+                Shopping on the Tata CLiQ App is an absolute delight. Download the <a href="#">Tata CLiQ Android app</a>
+                from the Play
+                Store or the <a href="#">iOS App</a> from Apple App Store and get set to enjoy a range of benefits. Apart
+                from the best
+                deals, amazing offers and the latest styles online, the app also gives you the flexibility to shop at
+                your convenience. Shop during a commute, in the middle of a holiday or at any time you want to. You can
+                also use the easy share options to share your shopping with your friends and family to ensure you're
+                buying something that's perfect. With constant updates and a host of new features being introduced
+                constantly, you also get to enjoy a shopping experience that you'll absolutely love. Download the Tata
+                CLiQ App today and prepare to rediscover the way you shop.<br><br>
+                <b>TATA CLiQ: Most genuine place for Electronics and Lifestyle Online Shopping, with India's most
+                    trusted destination. Buy Brands you love at prices you want!</b><br>
+                Shopping for your Lifestyle needs will be made delightfult with TATA CLiQ. With exclusive Brand store
+                for <a href="#">Westside Online</a> we have most of your trndy shopping needs taken care of. Make TATA CliQ
+                your online
+                clothes shopping destination and get the best deals on your favorite brands, with the 100% genuine
+                products. Be it gold coins or mobile phones online, you can count on TATA CliQ for receiving only the
+                most authentic products. Upgrade your lifestyle with shoes online shopping and online clothes shopping
+                only at TATA CLiQ. Online Shopping for men has never been more easy! Men like to choose Brands and keep
+                their wardrobe upto date with the latest trends. TATA CLiQ has made the Online shopping expirience
+                amazing for Men. Women's Lifetyle Online shopping needs have been taken care of as well, with the larget
+                assortment of womenwear online. <br>
+            </div>
+            </div>
+        </footer>`
+}
+
+export default footer
