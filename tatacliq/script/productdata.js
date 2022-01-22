6aade20a6c1385c2cb4702a1683ed08803d040d1
saurav <sauravsaran99@gmail.com>
2022-01-22 18:14:59 +0530

    product

---
+++ tatacliq/script/productdata.js
@@ -0,0 +1,51 @@
+let maiData = [{
+    name : 'Nuon Men',
+    title:'Nuon by Westside Charcoal Rodeo Carrot Fit Jeans',
+    price: 1499,
+    Newprice : 999,
+    url : 'https://img.tatacliq.com/images/i5/437Wx649H/MP000000006243509_437Wx649H_20200102084746.jpeg',
+}, {
+    name : 'Nuon Men',
+    title:'Nuon by Westside Charcoal Rodeo Carrot Fit Jeans',
+    price: 1499,
+    Newprice : 999,
+    url : 'https://img.tatacliq.com/images/i5/437Wx649H/MP000000006243509_437Wx649H_20200102084746.jpeg',
+}, {
+    name : 'Nuon Men',
+    title:'Nuon by Westside Charcoal Rodeo Carrot Fit Jeans',
+    price: 1499,
+    Newprice : 999,
+    url : 'https://img.tatacliq.com/images/i5/437Wx649H/MP000000006243509_437Wx649H_20200102084746.jpeg',
+}, {
+    name : 'Nuon Men',
+    title:'Nuon by Westside Charcoal Rodeo Carrot Fit Jeans',
+    price: 1499,
+    Newprice : 999,
+    url : 'https://img.tatacliq.com/images/i5/437Wx649H/MP000000006243509_437Wx649H_20200102084746.jpeg',
+}, {
+    name : 'Nuon Men',
+    title:'Nuon by Westside Charcoal Rodeo Carrot Fit Jeans',
+    price: 1499,
+    Newprice : 999,
+    url : 'https://img.tatacliq.com/images/i5/437Wx649H/MP000000006243509_437Wx649H_20200102084746.jpeg',
+}, {
+    name : 'Nuon Men',
+    title:'Nuon by Westside Charcoal Rodeo Carrot Fit Jeans',
+    price: 1499,
+    Newprice : 999,
+    url : 'https://img.tatacliq.com/images/i5/437Wx649H/MP000000006243509_437Wx649H_20200102084746.jpeg',
+}, {
+    name : 'Nuon Men',
+    title:'Nuon by Westside Charcoal Rodeo Carrot Fit Jeans',
+    price: 1499,
+    Newprice : 999,
+    url : 'https://img.tatacliq.com/images/i5/437Wx649H/MP000000006243509_437Wx649H_20200102084746.jpeg',
+}, {
+    name : 'Nuon Men',
+    title:'Nuon by Westside Charcoal Rodeo Carrot Fit Jeans',
+    price: 1499,
+    Newprice : 999,
+    url : 'https://img.tatacliq.com/images/i5/437Wx649H/MP000000006243509_437Wx649H_20200102084746.jpeg',
+}];
+
+export default maiData;
