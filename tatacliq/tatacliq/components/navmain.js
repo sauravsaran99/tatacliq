6909d95145efab3f498d908f24b0e312a04db7c7
Khushboo K <30701525+dipthic@users.noreply.github.com>
2022-01-23 08:28:08 +0530

    commit the folder

---
+++ tatacliq/tatacliq/components/navmain.js
@@ -0,0 +1,45 @@
+
+import navbar from "./navbar.js";
+
+let container = document.querySelector('#navbar');
+
+container.innerHTML = navbar();
+
+
+document.querySelector('.ravbox1').addEventListener('mouseover', display);
+
+document.querySelector('.ravbox1').addEventListener('mouseout', display1);
+
+document.querySelector('.two1').addEventListener('mouseover', display);
+
+    function display() {
+        document.querySelector('.ravbox1').style.display = 'flex';
+    }
+
+    document.querySelector('.two1').addEventListener('mouseout', display1);
+
+    function display1() {
+        document.querySelector('.ravbox1').style.display = 'none';
+    }
+
+
+
+
+    document.querySelector('.ravbox2').addEventListener('mouseover', display2);
+
+    document.querySelector('.ravbox2').addEventListener('mouseout', display3);
+    //second box
+document.querySelector('.two2').addEventListener('mouseover', display2);
+
+function display2() {
+    document.querySelector('.ravbox2').style.display = 'flex';
+}
+
+document.querySelector('.two2').addEventListener('mouseout', display3);
+
+function display3() {
+    document.querySelector('.ravbox2').style.display = 'none';
+}
+
+
+    
